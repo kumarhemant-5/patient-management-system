97e07fac7a7c296ec75c20b869f4ede20281755c
Hemant Kumar <hementkumaraja@gmail.com>
2026-03-15 00:20:48 +0530

    Some improvement done in service layer and added return type as reponseentity

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,7 +1,10 @@
 package com.pm.patientservice.service;
 
 import com.pm.patientservice.dao.PatientRepository;
+import com.pm.patientservice.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -11,11 +14,16 @@ public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
 
-    public void getPatientById(UUID id){
+    public ResponseEntity<String> getPatientById(UUID id){
+        String str;
         if(patientRepository.findById(id).isEmpty()){
-            System.out.println("Patient with this id is not present");
+            str = "Patient with this id is not present";
         }else{
-            System.out.println("Patient is present");
+            str = "Patient is present";
         }
+
+        // return new ResponseEntity<>(str,HttpStatus.OK);
+        // return ResponseEntity.ok(str);
+        return ResponseEntity.status(HttpStatus.OK).body(str);
     }
 }
