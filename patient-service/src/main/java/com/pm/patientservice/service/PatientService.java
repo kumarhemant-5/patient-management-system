871b888090ba24b1679edc66e58327f0889564a7
Hemant Kumar <hementkumaraja@gmail.com>
2026-03-14 21:32:11 +0530

    some repjetory bean is added in service layer

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,7 +1,11 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dao.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
 }
